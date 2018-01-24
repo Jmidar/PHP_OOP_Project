1e85f5c83a9b691ca3fa7f9846449cbdedf0b2eb
Arnab Das <arnab.das682@gmail.com>
2018-01-24 16:37:48 +0600

    Calculator 24/01/2018

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+class Calculation{
+
+	function add($a, $b){
+		echo "Summation = ".($a+$b)."<br>";
+	}
+
+	function sub($a, $b){
+		echo "Subtraction = ".($a-$b)."<br>";
+	}
+
+	function mul($a, $b){
+		echo "Multiplication = ".($a*$b)."<br>";
+	}
+
+	function div($a, $b){
+		echo "Divition = ".($a/$b)."<br>";
+	}
+
+	function mod($a, $b){
+		echo "Modulus = ".($a%$b)."<br>";
+	}
+
+}
+
+
+
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,64 +1,53 @@
-<?php include "inc/header.php"; ?>
-
-
 <?php 
+include "inc/header.php"; 
+include "functions.php"; 
 
-class Jmidar{
-	public $name;
-	public $age;
+?>
 
-	public function personName(){
-		 echo "My Name is ".$this->name="Jmidar";
-		
-	}
 
-	public function personAge($value){
+
+<form action="index.php" method="post">
+	<table>
+		<tr>
+			<td>Enter the 1st Number : </td>
+			<td><input type="number" name="num1" /></td>
+		</tr>
+
+		<tr>
+			<td>Enter the 2st Number : </td>
+			<td><input type="number" name="num2" /></td>
+		</tr>
+
+		<tr>
+			<td></td>
+			<td><input type="submit" name="calculation" value="calculate" /></td>
+		</tr>
+	</table>
+</form>
+
+<?php
+if(isset($_POST['calculation'])){
+
+	$numOne = $_POST['num1'];
+	$numTwo = $_POST['num2'];
+
+	if(empty($numOne) or empty($numTwo)){
+
+		echo "<span style='color:#EE6554'>Field must not be empty</span><br>";
+	} else {
+
+	$cal = new Calculation;
+	$cal->add($numOne, $numTwo);
+	$cal->sub($numOne, $numTwo);
+	$cal->mul($numOne, $numTwo);
+	$cal->div($numOne, $numTwo);
+	$cal->mod($numOne, $numTwo);
+
 
 	}
 
 }
-
-$jmi = new Jmidar;
-//echo $jmi->name;
-//$jmi->name="Jmidar";
-$jmi->personName();
-/*class Person{
-		public $name;
-		public $age;
-
-	    public function personName(){
-	    	echo "Person Name is:".$this->name."<br/>";
-	   }
-
-	  public function personAge($value){
-	  		echo "Person Age is:".$this->age=$value;
-
-	   }
-  }
-
-$personOne = new Person;
-$personOne->name="Ariful Islam";
-$personOne->personName();
-
-$personOne->personAge("18");
-
-*/
-
-
 ?>
-
-		
-
-
-
-
-
-
-
-
-
-
-
 
 
 <?php include "inc/footer.php"; ?>
